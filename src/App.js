ffb3db58febc8547387b8b54035906aafa5ff8d9
Divyanth Jayaraj <divyanthj@gmail.com>
2022-11-04 22:21:16 +0530

    Added some styling Created framework for implementing functionality

--- src/App.js
+++ src/App.js
@@ -2,28 +2,51 @@ import logo from './logo.svg';
 import Idea from "./Components/Idea";
 import { useState, useEffect } from "react";
 import IDEAS from "./Constants/API Urls/Ideas.js";
+import 'font-awesome/css/font-awesome.min.css';
 import './App.css';
 
 function App() {
 
   const [ ideas, setIdeas ] = useState([]);
+  const [ localIdeas, setLocalIdeas ] = useState([]);
 
   useEffect(() => {
     fetch(IDEAS).then(response => response.json()).then(result => {
-      setIdeas(result);
-      console.log("Ideas", ideas);
+      
+      localStorage.setItem("ideas", JSON.stringify(result));
+      setIdeas(JSON.parse(localStorage.getItem("ideas")));
     }).catch(error => {
       console.log("Error fetching ideas", error);
     })
   }, []);
 
+  const handleDeleteIdea = (id) => {
+
+  }
+
+  const handleUpdateIdea = (id) => {
+
+  }
+
+  const handleAddIdea = (title, body) => {
+
+  }
+
   return (
     <div className="App">
       {
         ideas && ideas.map(idea => {
-          return <Idea title={idea.title} body={idea.body} createdAt={idea.created_date} key={idea.id}></Idea>
+          return <Idea 
+                    title={idea.title} 
+                    body={idea.body} 
+                    createdAt={idea.created_date} 
+                    key={idea.id}></Idea>
+                    onDeleteIdea={handleDeleteIdea}
+                    onUpdateIdea={handleUpdateIdea}
+                    onAddIdea={handleAddIdea}
         })
       }
+      <Idea></Idea>
     </div>
   );
 }
--- src/Components/Idea.js
+++ src/Components/Idea.js
@@ -1,32 +1,94 @@
 import { useState, useEffect } from "react";
 import { createUseStyles } from 'react-jss'
 
+
 const styles = createUseStyles({
     ideaTile: {
         width: 200,
         height: 200,
         margin: 10,
         boxShadow: "1px 1px 5px #888888",
-        float: "left"
+        float: "left",
+        cursor: "pointer",
+        ':hover' : {
+            backgroundColor: "#aaaaaa"
+        }
     },
     title: {
         fontSize: 16,
         fontWeight: 600,
-        padding: 10
+        width: 180,
+        margin: 10,
+        border: 0
+    },
+    titleFocused: {
+        outlineColor: "#aaaaaa"
+    },
+    body: {
+        padding: 10,
+        border: 0
+    },
+    bodyFocused: {
+        outlineColor: "#aaaaaa"
     },
     createdAt: {
         fontSize: 9,
         color: "#888888"
+    },
+    deleteButton: {
+        boxShadow: "1px 1px 3px #888888",
+        borderRadius: 3,
+        width: 30,
+        height: 30,
+        float: "right",
+        margin: 10,
+        background: "grey",
+        color: "white"
     }
 })
 
-function Idea({ title, body, createdAt }) {
+function Idea({ title, body, createdAt, onAddIdea, onDeleteIdea, onUpdateIdea }) {
+    const isEmpty = !title && !body && !createdAt;
+    const [ isDeleteShown, setIsDeleteShown ] = useState();
+    const [ isTitleFocused, setIsTitleFocused ] = useState(false);
+    const [ isBodyFocused , setIsBodyFocused ] = useState(false);
     const classes = styles();
-    return (<div className={classes.ideaTile}>
-        <div className={classes.title}>{title}</div>
-        <div className={classes.createdAt}>{"Created on " + (new Date(createdAt))}</div>
-        <div>{body}</div>
 
+    const handleDelete = (title) => {
+        console.log("Deleting ", title);
+    }
+
+    const handleTitleFocus = (isFocused) => {
+        setIsTitleFocused(isFocused);
+    }
+
+    const handleBodyFocus = (isFocused) => {
+        setIsBodyFocused(isFocused);
+    }
+
+    const handleCreateNewIdea = () => {
+        console.log("Creating new idea");
+    }
+
+
+    return (!isEmpty ? <div className={classes.ideaTile} onMouseEnter={() => setIsDeleteShown(true)} onMouseLeave={() => setIsDeleteShown(false)}>
+        <input 
+            className={classes.title + " " + (isTitleFocused && classes.titleFocused)} 
+            value={title}
+            onFocus={() => handleTitleFocus(true)}
+            onBlur={() => handleTitleFocus(false)}></input>
+        <div className={classes.createdAt}>{"Created on " + (new Date(createdAt))}</div>
+        <textarea className={classes.body + " " + (isBodyFocused && classes.bodyFocused)} 
+            value={body}
+            onFocus={() => handleBodyFocus(true)}
+            onBlur={() => handleBodyFocus(false)} rows={4}></textarea>
+        {
+            isDeleteShown && <div className={classes.deleteButton} onClick={() => handleDelete(title)}>
+                <i className="fa fa-trash" ></i>
+            </div>}
+    </div> : 
+    <div className={classes.ideaTile} onClick={() => this.handleCreateNewIdea()}>
+        <h2>+</h2>
     </div>)
 }
 
